c82b3b0f9e6e3151d4ba05eb6b4eeac208182ff1
SparKo <107945330+xSparKo@users.noreply.github.com>
2024-03-28 21:58:21 +0100

    Use SwiftUI view from MapKit to render the map

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by SparKo on 28.03.2024.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
