103a90dec42060436df2bda3e37446c040fd262a
SparKo <107945330+xSparKo@users.noreply.github.com>
2024-03-31 21:48:40 +0200

    Pass data into child views

--- Landmarks/Views/LandmarkDetail.swift
+++ Landmarks/Views/LandmarkDetail.swift
@@ -1,46 +1,43 @@
-//
-//  LandmarkDetail.swift
-//  Landmarks
-//
-//  Created by SparKo on 31.03.2024.
-//
-
 import SwiftUI
 
 struct LandmarkDetail: View {
+    var landmark: Landmark
+
     var body: some View {
-        VStack {
-            MapView()
+        ScrollView {
+            MapView(coordinate: landmark.locationCoordinate)
                 .frame(height: 300)
-            
-            CircleImage()
+            CircleImage(image: landmark.image)
                 .offset(y: -130)
                 .padding(.bottom, -130)
-            
-            VStack (alignment: .leading) {
-                Text("Turtle Rock")
+
+            VStack(alignment: .leading) {
+                Text(landmark.name)
                     .font(.title)
-                
+
                 HStack {
-                    Text("Joshua Tree National Park")
+                    Text(landmark.park)
                     Spacer()
-                    Text("California")
+                    Text(landmark.state)
                 }
+                
                 .font(.subheadline)
                 .foregroundStyle(.secondary)
-                
-                
+
                 Divider()
-                Text("About Turtle Rock")
-                    .font(.title)
-                Text("Descriptive text goes here")
+
+                Text("About \(landmark.name)")
+                    .font(.title2)
+
+                Text(landmark.description)
             }
             .padding()
-            Spacer()
         }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    LandmarkDetail()
+    LandmarkDetail(landmark: landmarks[0])
 }
